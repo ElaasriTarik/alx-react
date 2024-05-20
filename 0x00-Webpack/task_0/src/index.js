7692f82664e49e8bbac0de4fc27440c5107c9c49
ElaasriTarik <elaasriya2003@gmail.com>
2024-05-20 16:44:14 +0100

    webpack 1

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+import $ from "jquery";
+
+// adding 3 paragraphs to the body
+$("body").append($("<p>").text(`Holberton Dashboard`));
+$("body").append($("<p>").text(`Dashboard data for the students`));
+$("body").append($("<p>").text(`Copyright - Holberton School`));
